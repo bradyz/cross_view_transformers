99b64c9189ce6f0ea3ab0e3fed074baeefbb81ef
bradyz <brady.zhou@utexas.edu>
2022-05-05 07:32:55 -0500

    remove

--- cross_view_transformer/data/__init__.py
+++ cross_view_transformer/data/__init__.py
@@ -1,9 +1,3 @@
-from . import lyft_dataset
-from . import lyft_dataset_generated
-
-from . import argoverse_dataset
-from . import argoverse_dataset_generated
-
 from . import nuscenes_dataset
 from . import nuscenes_dataset_generated
 
@@ -11,12 +5,6 @@ from . import nuscenes_dataset_generated
 MODULES = {
     'nuscenes': nuscenes_dataset,
     'nuscenes_generated': nuscenes_dataset_generated,
-
-    'lyft': lyft_dataset,
-    'lyft_generated': lyft_dataset_generated,
-
-    'argoverse': argoverse_dataset,
-    'argoverse_generated': argoverse_dataset_generated,
 }
 
 
