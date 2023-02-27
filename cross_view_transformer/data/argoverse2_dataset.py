d57b003b7dc7681f01f7f6ab0536f3a404620ece
Adam Patni <adampatni27@gmail.com>
2023-02-27 14:58:31 -0500

    got bev image working, now trying to get the binary masks nailed down

--- cross_view_transformer/data/argoverse2_dataset.py
+++ cross_view_transformer/data/argoverse2_dataset.py
@@ -25,44 +25,64 @@ from av2.utils.io import read_city_SE3_ego, read_img
 import random
 
 
-STATIC = [
-    AnnotationCategories.BOLLARD ,
-    AnnotationCategories.CONSTRUCTION_BARREL ,
-    AnnotationCategories.CONSTRUCTION_CONE ,
-    AnnotationCategories.MOBILE_PEDESTRIAN_CROSSING_SIGN ,
-    AnnotationCategories.SIGN ,
-    AnnotationCategories.STOP_SIGN ,
-    AnnotationCategories.TRAFFIC_LIGHT_TRAILER ,
-    AnnotationCategories.MOBILE_PEDESTRIAN_CROSSING_SIGN ,
+CLASS_TO_LAYER_INDEX: Dict[Any,int] = {
+    "roi": 0,
+    "drivable_area": 1,
+    "lane_boundary": 2,
+    AnnotationCategories.RAILED_VEHICLE : 3,
+    AnnotationCategories.REGULAR_VEHICLE : 3,
+    AnnotationCategories.LARGE_VEHICLE : 3,
+    
+    AnnotationCategories.BOX_TRUCK : 4,
+    AnnotationCategories.TRUCK_CAB : 4,
+    AnnotationCategories.TRUCK : 4,
 
-          ]
-DYNAMIC = [
-    AnnotationCategories.ARTICULATED_BUS ,
-    AnnotationCategories.BICYCLE ,
-    AnnotationCategories.ANIMAL ,
-    AnnotationCategories.BICYCLIST ,
-    AnnotationCategories.BOX_TRUCK ,
-    AnnotationCategories.BUS ,
-    AnnotationCategories.DOG ,
-    AnnotationCategories.LARGE_VEHICLE ,
-    AnnotationCategories.MESSAGE_BOARD_TRAILER ,
-    AnnotationCategories.MOTORCYCLE ,
-    AnnotationCategories.MOTORCYCLIST ,
-    AnnotationCategories.OFFICIAL_SIGNALER ,
-    AnnotationCategories.PEDESTRIAN ,
-    AnnotationCategories.BICYCLIST ,
-    AnnotationCategories.RAILED_VEHICLE ,
-    AnnotationCategories.REGULAR_VEHICLE ,
-    AnnotationCategories.SCHOOL_BUS ,
-    AnnotationCategories.STROLLER ,
-    AnnotationCategories.TRUCK ,
-    AnnotationCategories.TRUCK_CAB ,
-    AnnotationCategories.VEHICULAR_TRAILER ,
-    AnnotationCategories.WHEELCHAIR ,
-    AnnotationCategories.WHEELED_RIDER ,
-    AnnotationCategories.WHEELED_DEVICE ,
-          ]
-NUM_CLASSES = len(STATIC) + len(DYNAMIC)
+    AnnotationCategories.ARTICULATED_BUS : 5,
+    AnnotationCategories.BUS : 5,
+    AnnotationCategories.SCHOOL_BUS : 5,
+
+    AnnotationCategories.VEHICULAR_TRAILER : 6,
+    
+    AnnotationCategories.BOLLARD: 7,
+    AnnotationCategories.CONSTRUCTION_BARREL : 7,
+    AnnotationCategories.CONSTRUCTION_CONE : 7,
+    
+    AnnotationCategories.PEDESTRIAN : 8,
+    AnnotationCategories.OFFICIAL_SIGNALER : 8,
+    AnnotationCategories.STROLLER : 8,
+    AnnotationCategories.WHEELCHAIR : 8,
+
+    AnnotationCategories.MOTORCYCLIST : 9,
+    AnnotationCategories.MOTORCYCLE : 9,
+    
+    AnnotationCategories.WHEELED_RIDER : 10,
+    AnnotationCategories.WHEELED_DEVICE : 10,
+    AnnotationCategories.BICYCLE : 10,
+    AnnotationCategories.BICYCLIST : 10,    
+
+    # NOT INCLUDED AT THE MOMENT
+    # AnnotationCategories.MOBILE_PEDESTRIAN_CROSSING_SIGN : 1,
+    # AnnotationCategories.SIGN : 1,
+    # AnnotationCategories.STOP_SIGN : 1,
+    # AnnotationCategories.TRAFFIC_LIGHT_TRAILER : 1,
+    # AnnotationCategories.MESSAGE_BOARD_TRAILER : 1,
+    # AnnotationCategories.ANIMAL : 1,
+    # AnnotationCategories.DOG : 1,
+}
+NUM_LAYERS = max(CLASS_TO_LAYER_INDEX.values())+1
+COLORING = {
+    0: (0,0,0),
+    1: (105,105,105),
+    2: (0,0,0),
+    3: (35,13,232),
+    4: (157,30,150),
+    5: (251,207,34),
+    6: (138,89,196),
+    7: (255,147,29),
+    8: (189,142,0),
+    9: (113,182,255),
+    10: (133,41,107),
+}
 
 def get_data(
     dataset_dir,
@@ -78,14 +98,7 @@ def get_data(
     dataset_dir = Path(dataset_dir)
     labels_dir = Path(labels_dir)
 
-    transform = SaveDataTransform(labels_dir)
-
-    # Format the split name
-    # split = f"mini_{split}" if version == "mini" else split
-
     log_ids = get_split(f"mini_{split}" if version == "mini" else split, "argoverse2") # TODO: modify for argopaths (actually this seems fine)
-
-    # av2_dataloader = AV2SensorDataLoader(dataset_dir, dataset_dir)
 
     sensor_dataloader = SensorDataloader(
             dataset_dir=dataset_dir,
@@ -158,62 +171,97 @@ class Argoverse2Dataset(torch.utils.data.Dataset):
             )
         return data
 
-    def generate_bev(self, pose: SE3, timestamp: int) -> Tuple[np.ndarray, np.ndarray]:
-        pose_inv = pose.inverse()
-        xpoints = np.arange(-self.bev_info['h_meters']/2,self.bev_info['h_meters']/2,self.bev_info['h_meters']/self.bev_info['h'])
-        ypoints = np.arange(-self.bev_info['w_meters']/2,self.bev_info['w_meters']/2,self.bev_info['w_meters']/self.bev_info['w'])
-        zpoints = np.array([i/10.0 for i in range(20)])
-        # xcoord, ycoord, z_coord= np.meshgrid(xpoints, ypoints)
-        # xyz_coords = np.concatenate((xcoord, ycoord, z_coord),axis=2)
-        # points_xy_city = location.transform_point_cloud(xyz_coords)
+    def point_cloud_to_bev(
+        self,
+        points: np.ndarray,
+        layers_masks: np.ndarray,
+        coloring: Dict[int, Tuple],
+        img_size: Tuple[int, int]
+        ) -> Tuple[np.ndarray, np.ndarray]:
+        """Creates a 2D BEV image from the point cloud.
+        
+        Note that the order of layers determines which color is on top
 
+        Args:
+            points (np.ndarray): (N,3)
+            layers_masks (np.ndarray): (L,N)
+            coloring (Dict[int, Tuple]): Each layer's respective color (r,g,b)
+            img_size (Tuple): (height, width)
+
+        Returns:
+            Tuple[np.ndarray, np.ndarray]: image of size (H,W, 3) AND binary masks for each layer (L, H, W)
+        """       
+        assert points.shape[-1] == 3
+        assert points.shape[0] == layers_masks.shape[-1]
+        x_points = points[:, 0].astype(np.float32)
+        y_points = points[:, 1].astype(np.float32)
+
+        min_x = np.min(x_points)
+        min_y = np.min(y_points)
+        max_x = np.max(x_points)
+        max_y = np.max(y_points)
+
+        # Ensures that the grid is equally wide on each side
+        # This is not particularly necessary, but it's a good 
+        # check for this specific use case
+        assert abs(min_x) == abs(max_x) 
+        assert abs(min_y) == abs(max_y)
+        
+
+        # Left/Upper Adjust all the points to be in the image coordinate from
+        x_points -= min_x
+        y_points -= min_y
+        
+        # Scale the point cloud onto the image plane 
+        res_x = (max_x - min_x)/(img_size[1]-1)
+        res_y = (max_y - min_y)/(img_size[0]-1)
+
+        x_img = (-y_points / res_y).astype(np.int32)  # x axis is -y in LIDAR
+        y_img = (-x_points / res_x).astype(np.int32)  # y axis is -x in LIDAR
+
+        # Shift Pixels to upper left corner (after division)
+        # floor & ceil used to prevent anything being rounded to below 0 after shift
+        x_img -= int(np.floor(np.min(x_img)))
+        y_img -= int(np.ceil(np.min(y_img)))
+        
+        # Build Image and Binary Semantic Segmentation Masks
+        im = np.zeros([img_size[0], img_size[1], 3], dtype=np.uint8)
+        binary_semantic_seg = np.zeros((len(coloring), img_size[0], img_size[1]))
+
+        # Fill in pixels for image/mask
+        for i in range(len(coloring)):
+            im[y_img[layers_masks[i]==1], x_img[layers_masks[i]==1]] = coloring[i]
+            binary_semantic_seg[i, y_img[layers_masks[i]==1], x_img[layers_masks[i]==1]] = 1
+
+        return im, binary_semantic_seg
+
+
+    def generate_bev(self, pose: SE3, timestamp: int) -> Tuple[np.ndarray, np.ndarray]:
+        xpoints = np.linspace(-self.bev_info['h_meters']/2,self.bev_info['h_meters']/2,num=self.bev_info['h'])
+        ypoints = np.linspace(-self.bev_info['w_meters']/2,self.bev_info['w_meters']/2,num=self.bev_info['w'])
+        zpoints = np.linspace(-1,4, num=15)
+        
         # note: may have to change map() to list() if numpy upgraded >= 1.16
         points_xy_wrt_src = np.vstack(map(np.ravel, np.meshgrid(xpoints, ypoints, zpoints))).T
-        points_xy_wrt_city = pose.transform_point_cloud(points_xy_wrt_src) 
-        # Note that points_xy_wrt_city is a list of all the points (X,Y,Z) coords, not a 2D grid
+        points_xy_wrt_city = pose.transform_point_cloud(points_xy_wrt_src)
+        # TODO: consider removing all non-ROI points (assuming everything else works this can save on computation) 
         
-        # # Project cuboids onto raster
-        # dynamic_raster = np.zeros(shape=(points_xy_wrt_city.shape[0]))
-        # static_raster = np.zeros(shape=(points_xy_wrt_city.shape[0]))
+        output = np.zeros((NUM_LAYERS, self.bev_info['h'], self.bev_info['w']))
+        layer_masks = np.zeros((NUM_LAYERS, points_xy_wrt_city.shape[0]))
         
-        # CMAP index -> color mapping for each point in city (N,3) (this determines dynamic vs static etc in the bev)
-        cmap = np.zeros(shape=(points_xy_wrt_city.shape[0], 3))
-        # Get driveable area raster (N,3) = (num points, (x,y,z))
         drivable_area_raster = self.avm.raster_drivable_area_layer.get_raster_values_at_coords(points_xy_wrt_city, 0)
-        cmap[drivable_area_raster==0] = (0,0,0)
-        cmap[drivable_area_raster==1] = (220,220,220)
-
+        layer_masks[1,drivable_area_raster==1] = 1
 
         for annotation in CuboidList.from_feather(Path(self.log_specific_dataset_path, 'annotations.feather')):
-            if annotation.timestamp_ns == timestamp:
-                annotation = annotation.transform(pose)      
-                _, is_interior = annotation.compute_interior_points(points_xy_wrt_city)          
-                if annotation.category in STATIC:
-                    # print("static")
-                    # print(interior_points)
-                    # print(annotation.dst_SE3_object.translation)
-                    # print(annotation.width_m, annotation.length_m, annotation.height_m)
-                    # print(np.count_nonzero(is_interior))
-                    cmap[is_interior] = (255,127,80)            
-                elif annotation.category in DYNAMIC:
-                    # print("dynamic")
-                    # print(interior_points)
-                    # print(annotation.dst_SE3_object.translation)
-                    # print(annotation.width_m, annotation.length_m, annotation.height_m)
-                    # print(np.count_nonzero(is_interior))
-                    cmap[is_interior] = (30,144,255)
+            if annotation.timestamp_ns == timestamp and annotation.category in CLASS_TO_LAYER_INDEX:
+                _, is_interior = annotation.compute_interior_points(points_xy_wrt_src)          
+                layer_masks[CLASS_TO_LAYER_INDEX[annotation.category], is_interior] = 1
 
+        image_bev, binary_semantic_masks = self.point_cloud_to_bev(points_xy_wrt_src,layer_masks,COLORING,(self.bev_info['h'],self.bev_info['w']))
 
-        # Convert the Driveable Area Raster + Dynamic + Static Map into a BEV Image
-        output_bev = xyz_to_bev(
-            xyz=points_xy_wrt_city,
-            voxel_resolution=(self.bev_info['h'], self.bev_info['w'], 1),
-            grid_size_m=(self.bev_info['h_meters'], self.bev_info['w_meters'], 1),
-            cmap=cmap
-        )
-        Image.save(output_bev, f"/srv/share2/apatni30/cvt_labels_argoverse2/{self.log_id}_{random.randint(1, 100)}.png")
+        Image.save(image_bev, f"/srv/share2/apatni30/cvt_labels_argoverse2/{self.log_id}_{timestamp}.png")
 
-        return output_bev
+        return image_bev, binary_semantic_masks
 
     def __len__(self):
         return len(self.samples)
@@ -223,11 +271,11 @@ class Argoverse2Dataset(torch.utils.data.Dataset):
 
         # Additional labels for vehicles only.
         # aux, visibility = self.get_dynamic_objects(sample, anns_vehicle)
-
+        bev_img, bev_masks = self.generate_bev(sample['pose'], sample['token'])
         # Package the data.
         data = Sample(
             view=self.view.tolist(),
-            bev=self.generate_bev(sample['pose'], sample['token']),
+            bev=bev_masks,
             # aux=aux,
             # visibility=visibility,
             **sample
